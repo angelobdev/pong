d0d8dcd9e6a032276c0243f002195cbd85f99f96
Angelo Belcastro <angelo.belcastro01@gmail.com>
2023-01-24 16:02:59 +0100

    Updated Game

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Pong
+# Pong
+
+Classic old-fashioned game.  
+Written in JavaScript using the [p5.js](https://p5js.org/) framework.
+
+---
+
+Live demo can be found here: [DEMO](https://angelobdev.github.io/projects/pong/)
--- pong.js
+++ pong.js
@@ -1,4 +1,5 @@
-function getAngle() {
+/* ***** GLOBAL METHODS ***** */
+function genRandomAngle() {
   var xDir = random(1) > 0.5 ? true : false;
   var yDir = random(1) > 0.5 ? true : false;
   var angle = random(PI / 6);
@@ -9,20 +10,52 @@ function getAngle() {
   else if (xDir && yDir) return 2 * PI - angle;
 }
 
-// Ball class
+function renderPlayingField() {
+  background(64, 200, 78);
+  // Drawing field lines
+  noFill();
+  stroke(255, 255, 128);
+  strokeWeight(3);
+  line(width / 2, 0, width / 2, height);
+  circle(width / 2, height / 2, height / 2);
+  arc(0, height / 2, 300, 300, (3 * PI) / 2, PI / 2);
+  arc(width, height / 2, 300, 300, PI / 2, (3 * PI) / 2);
+}
+
+function renderScores() {
+  if (!paused) {
+    // Drawing scores
+    fill(255);
+    stroke(255);
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    text(leftScore, width / 2 - 48, height / 2);
+    text(rightScore, width / 2 + 48, height / 2);
+  } else {
+    // Drawing "PAUSED"
+    fill(255);
+    stroke(0);
+    textSize(72);
+    textAlign(CENTER, CENTER);
+    text("PAUSED", width / 2, height / 2);
+  }
+}
+
+/* ***** BALL CLASS ***** */
 class Ball {
   constructor(x, y, r, speed) {
     this.x = x - r / 2;
     this.y = y - r / 2;
     this.r = r;
+
     this.speed = speed;
-    this.angle = getAngle();
+    this.angle = PI / 4;
   }
 
   reset() {
     this.x = width / 2 - this.r / 2;
     this.y = height / 2 - this.r / 2;
-    this.angle = getAngle();
+    this.angle = genRandomAngle();
   }
 
   update() {
@@ -45,21 +78,24 @@ class Ball {
   }
 
   render() {
-    fill(220);
+    fill(255);
     stroke(0);
     ellipse(this.x, this.y, this.r);
   }
 }
-//Racket
+
+/* ***** RACKET CLASS ***** */
 class Racket {
   constructor(x, y) {
     this.x = x;
     this.y = y;
     this.w = width * 0.01;
-    this.h = height * 0.4;
+    this.h = height * 0.3;
 
-    this.acc = 1;
+    this.acc = 0.8;
     this.speed = 0;
+
+    this.touching = false;
   }
 
   move(dir) {
@@ -67,53 +103,51 @@ class Racket {
     this.y += this.speed * dir;
   }
 
+  checkCollision(ball) {
+    var dx = Math.abs(ball.x - this.x); // Distance on X axis
+    var dy = Math.abs(ball.y - this.y); // Distance on Y axis
+
+    if (!this.touching) {
+      if (dx <= this.w / 2 + ball.r / 2 && dy <= this.h / 2 + ball.r / 2) {
+        this.touching = true;
+        ball.angle = PI - ball.angle;
+        ball.speed += 0.1;
+      }
+    } else {
+      setTimeout(() => {
+        // Waiting to avoid undesired effects
+        this.touching = false;
+      }, 1000);
+    }
+  }
+
   update() {
-    //HEIGHT CONSTRAINS
+    // HEIGHT CONSTRAINS
     if (this.y < this.h / 2) this.y = this.h / 2;
     else if (this.y > height - this.h / 2) {
       this.y = height - this.h / 2;
     }
 
-    //RIGHT PUCK CONTROLLER
-    if (this.x > width / 2) {
-      if (ball.y > this.h / 2 && ball.y < height - this.h / 2)
-        if (this.y < ball.y) this.y += 0.4;
-        else this.y -= 0.4;
-      //BALL COLLISION RIGHT
-      if (
-        ball.y < this.y + this.h / 2 &&
-        ball.y > this.y - this.h / 2 &&
-        ball.x + ball.r / 2 > this.x - this.w / 2
-      ) {
-        ball.speed *= -1;
-        ball.angle *= -1;
-      }
-    } else {
-      //BALL COLLISION LEFT
-      if (
-        ball.y < this.y + this.h / 2 &&
-        ball.y > this.y - this.h / 2 &&
-        ball.x - ball.r / 2 < this.x + this.w / 2
-      ) {
-        ball.speed *= -1;
-        ball.angle *= -1;
-      }
-      //speed decreaser
-      this.speed *= 0.9;
-    }
+    // Decreasing speed
+    this.speed *= 0.8;
   }
 
   render() {
-    if (this.x > width / 2) fill(255, 0, 0);
-    else fill(0, 0, 255);
+    if (this.x > width / 2) fill(255, 128, 128);
+    else fill(128, 128, 255);
+
+    rectMode(CENTER);
+
     stroke(0);
     strokeWeight(1);
-    rect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
+    rect(this.x, this.y, this.w, this.h, 20);
   }
 }
 
-//Game
+/* ***** ***** GAME ***** ***** */
 let ball;
+let ballStartSpeed = 5;
+
 let leftR;
 let rightR;
 
@@ -121,42 +155,79 @@ var border;
 
 var leftScore = 0;
 var rightScore = 0;
+function resetScore() {
+  leftScore = 0;
+  rightScore = 0;
+  ball.reset();
+  ball.speed = ballStartSpeed;
+}
+
+var paused = false;
 
 function setup() {
-  createCanvas(700, 400);
-  ball = new Ball(width / 2, height / 2, 20, 2);
+  // Creating canvas
+  createCanvas((3 * screen.width) / 6, (2 * screen.height) / 5);
+
+  // Setting variables
   border = width * 0.02;
+
+  // Creating ball and rackets
+  ball = new Ball(width / 2, height / 2, 20, ballStartSpeed);
   leftR = new Racket(border, height / 2);
   rightR = new Racket(width - border, height / 2);
 }
 
 function draw() {
-  background(0, 255, 0);
+  //Processing inputs
+  if (keyIsDown(81)) {
+    // Q
+    leftR.move(-1);
+  }
+  if (keyIsDown(65)) {
+    // A
+    leftR.move(1);
+  }
+  if (keyIsDown(80)) {
+    // P
+    rightR.move(-1);
+  }
+  if (keyIsDown(76)) {
+    // L
+    rightR.move(1);
+  }
 
-  noFill();
-  stroke(255);
-  strokeWeight(1);
-  text(leftScore, width * 0.2, height * 0.2);
-  text(rightScore, width * 0.8, height * 0.2);
-
-  noFill();
-  stroke(255, 255, 0);
-  strokeWeight(4);
-  line(width / 2, 0, width / 2, height);
-  circle(width / 2, height / 2, height / 2);
-
+  // Updating
+  leftR.checkCollision(ball);
   leftR.update();
+
+  rightR.checkCollision(ball);
   rightR.update();
+
   ball.update();
+
+  /* RENDERING */
+  renderPlayingField();
+  renderScores();
+
+  stroke(0);
+  fill(255);
+  textSize(24);
+  textAlign(CENTER, CENTER);
+  text("Difficulty: " + ball.speed.toFixed(1), width / 2, height * 0.95);
 
   leftR.render();
   rightR.render();
   ball.render();
+}
 
-  if (keyIsDown(UP_ARROW)) {
-    leftR.move(-1);
+function keyPressed() {
+  if (key === "r") {
+    resetScore();
   }
-  if (keyIsDown(DOWN_ARROW)) {
-    leftR.move(1);
+
+  if (key === " ") {
+    if (paused) loop();
+    else noLoop();
+    paused = !paused;
   }
 }
